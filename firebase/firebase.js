b33c171a4fcbf4f7ce67999253928ab32629c3c0
Anya Ji <aj592@cornell.edu>
2021-05-10 22:50:24 -0400

    config

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -9,12 +9,12 @@ import "firebase/auth";
 // import { firebaseConfig } from "./config";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCy4rUt-9WoWWqI8PpsMS9eJsTtgccse9w",
-  authDomain: "image-upload-app-f6c0e.firebaseapp.com",
-  projectId: "image-upload-app-f6c0e",
-  storageBucket: "image-upload-app-f6c0e.appspot.com",
-  messagingSenderId: "721362973684",
-  appId: "1:721362973684:web:456fe3f9922375033abfa1",
+  apiKey: "AIzaSyC7VKTiT_61QOdGH08Tm1xA2uWtGNOZePA",
+  authDomain: "image-upload-app-c2b16.firebaseapp.com",
+  projectId: "image-upload-app-c2b16",
+  storageBucket: "image-upload-app-c2b16.appspot.com",
+  messagingSenderId: "2925612514",
+  appId: "1:2925612514:web:4d74fd413fb2b2aeecfdbb"
 };
 
 if (!firebase.apps.length) {
