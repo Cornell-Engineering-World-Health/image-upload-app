ce34e4435c3874821c1e27bc1f27742af42b4588
michaelzhang0608 <61032096+michaelzhang0608@users.noreply.github.com>
2021-09-29 19:17:18 -0400

    finished upload screen design (#22)
    * preliminary upload screen new design
    
    * almost finished upload screen
    
    * labels screen complete
    
    * fix merge conflict
    
    * fixed UI bug
    
    * made textinput larger for easier access
    
    * fixed merge conflicts
    
    * try to fix merge conflicts
    
    * Update home.js
    
    * Update home.js
    
    Co-authored-by: tklausner <34672984+tklausner@users.noreply.github.com>

--- screens/upload.js
+++ screens/upload.js
@@ -2,15 +2,20 @@ import * as React from "react";
 import { useState, useContext } from "react";
 import {
   TouchableOpacity,
+  TouchableHighlight,
   StyleSheet,
   View,
   Text,
+  ScrollView,
   Image,
+  Button,
   TextInput,
 } from "react-native";
 import NextButton from "../components/nextButton";
 import { Image_object, Metadata } from "../util/Image";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { WhiteBalance } from "expo-camera/build/Camera.types";
+import { Right } from "native-base";
 //import ImageViewer from 'react-native-image-zoom-viewer';
 import { UserContext } from "../util/context";
 
@@ -27,46 +32,166 @@ import { UserContext } from "../util/context";
 function UploadScreen({ route, navigation }) {
   const { img } = route.params;
   console.log(img.uri);
-  const [category1, newCategory1] = useState("");
-  const [category2, newCategory2] = useState("");
-  const [category3, newCategory3] = useState("");
+  const tags = ["Label1", "Label2", "Label3", "Label4", "Label5", "Label6", "Label7",
+    "Label8", "Label9", "Label10", "Label11", "Label12", "Label13", "Label14"]
+  const [selectedTags, setSelectedTags] = useState({ arr: Array(tags.length).fill(false) })
+  const [tagButtonStyles, setTagButtonStyle] = useState({
+    arr: Array(tags.length).fill({})
+  })
+  const [tagTextStyles, setTagTextStyles] = useState({
+    arr: Array(tags.length).fill({})
+  })
+  const [tagRectangleStyles, setTagRectangleStyles] = useState({
+    arr: Array(tags.length).fill({})
+  })
+  const [customTags, setCustomTags] = useState(["", "", ""])
+  const [addTagButtonStyle, setCustomTagButtonStyle] = useState({ customTags: [style.tagButton] })
+  const [addTagTextStyle, setCustomTagTextStyle] = useState({ customTags: [style.textInput] })
+  const [addTagRectangleStyle, setCustomTagRectangleStyle] = useState({ customTags: [style.rectangle] })
   const [state, dispatch] = useContext(UserContext);
 
+  const addMore = () => {
+    let addTagRectangleStyleTemp = addTagRectangleStyle.customTags
+    addTagRectangleStyleTemp[addTagRectangleStyleTemp.length - 1] = style.selectedRectangle
+    addTagRectangleStyleTemp.push(style.rectangle)
+    setCustomTagRectangleStyle({ customTags: addTagRectangleStyleTemp })
+    let addTagTextStyleTemp = addTagTextStyle.customTags
+    addTagTextStyleTemp[addTagTextStyleTemp.length - 1] = style.selectedTagText
+    addTagTextStyleTemp.push(style.textInput)
+    setCustomTagTextStyle({ customTags: addTagTextStyleTemp })
+    let addTagButtonStyleTemp = addTagButtonStyle.customTags
+    addTagButtonStyleTemp[addTagButtonStyleTemp.length - 1] = style.selectedTagButton
+    addTagButtonStyleTemp.push(style.tagButton)
+    setCustomTagButtonStyle({ customTags: addTagButtonStyleTemp })
+  }
+
+  var length = 0;
+  var tagList = [];
+  var threeOrFour = true;
+  while (length < tags.length) {
+    if (threeOrFour) {
+      var newList = []
+      var i = 4;
+      while (length < tags.length && i > 0) {
+        newList.push(tags[length])
+        i--
+        length++
+      }
+      tagList.push(newList)
+      threeOrFour = false;
+    } else {
+      var newList = []
+      var i = 3;
+      while (length < tags.length && i > 0) {
+        newList.push(tags[length])
+        i--
+        length++
+      }
+      tagList.push(newList)
+      threeOrFour = true;
+    }
+  }
+
+  let newTag = ["0", "1", "2"].map((item, index) => {
+    return (
+      <View style={addTagRectangleStyle.customTags[index]}>
+        <TouchableHighlight style={addTagButtonStyle.customTags[index]}>
+          <View>
+            <TextInput style={addTagTextStyle.customTags[index]} onChangeText={(customTag) => {
+              let temp = customTags;
+              temp[index] = customTag
+              setCustomTags(temp);
+            }}>
+            </TextInput>
+          </View>
+        </TouchableHighlight>
+      </View>
+    )
+  })
   return (
     <SafeAreaView style={style.view}>
       <Text style={style.label}>Label Image</Text>
       <Image style={style.thumbnail} source={{ uri: img.uri }} />
-      <Text style={style.tags_label}>Tags:</Text>
-      <View style={style.tags_view}>
-        <View style={style.rectangle}>
-          <TextInput
-            style={style.input}
-            onChangeText={(x) => newCategory1(x)}
-            placeholderTextColor="black"
-            placeholder="    Category 1"
-          ></TextInput>
+      <ScrollView style={style.scrollView}>
+        <View style={style.scrollViewHeaderView}>
+          <Text style={style.scrollViewHeader}>Select a label from the following list:</Text>
         </View>
-        <View style={style.rectangle2}>
-          <TextInput
-            style={style.input}
-            onChangeText={(x) => newCategory2(x)}
-            placeholderTextColor="black"
-            placeholder="    Category 2"
-          ></TextInput>
+        <View style={{ flexDirection: "column" }}>
+          {tagList.map((item) =>
+            <View style={{ flexDirection: "row" }}>
+              {item.map((tag) => {
+                return <View style={tagRectangleStyles[tags.indexOf(tag)] || style.rectangle}>
+                  <TouchableHighlight
+                    onPress={function () {
+                      let selectedTagsTemp = selectedTags.arr;
+                      selectedTagsTemp[tags.indexOf(tag)] = !selectedTagsTemp[tags.indexOf(tag)];
+                      setSelectedTags({ arr: selectedTagsTemp })
+                      let tagButtonStylesTemp = tagButtonStyles
+                      if (selectedTagsTemp[tags.indexOf(tag)]) {
+                        tagButtonStylesTemp[tags.indexOf(tag)] = style.selectedTagButton
+                      } else {
+                        tagButtonStylesTemp[tags.indexOf(tag)] = style.tagButton
+                      }
+                      setTagButtonStyle(tagButtonStylesTemp)
+                      let tagTextStylesTemp = tagTextStyles
+                      if (selectedTagsTemp[tags.indexOf(tag)]) {
+                        tagTextStylesTemp[tags.indexOf(tag)] = style.selectedTagText
+                      } else {
+                        tagTextStylesTemp[tags.indexOf(tag)] = style.tagText
+                      }
+                      setTagTextStyles(tagTextStylesTemp)
+                      let tagRectangleStylesTemp = tagRectangleStyles
+                      if (selectedTagsTemp[tags.indexOf(tag)]) {
+                        tagRectangleStylesTemp[tags.indexOf(tag)] = style.selectedRectangle
+                      } else {
+                        tagRectangleStylesTemp[tags.indexOf(tag)] = style.rectangle
+                      }
+                      setTagRectangleStyles(tagRectangleStylesTemp)
+                    }}
+                    style={tagButtonStyles[tags.indexOf(tag)] || style.tagButton}>
+                    <Text style={tagTextStyles[tags.indexOf(tag)] || style.tagText}>{tag}</Text>
+                  </TouchableHighlight>
+                </View>
+              })}
+            </View>)}
+          <View style={{ flexDirection: "row" }}>
+            {newTag}
+          </View>
+
         </View>
-        <View style={style.rectangle3}>
-          <TextInput
-            style={style.input}
-            onChangeText={(x) => newCategory3(x)}
-            placeholderTextColor="black"
-            placeholder="    Category 3"
-          ></TextInput>
+        <View style={{
+          height: 45,
+          width: 69,
+          marginTop: 10,
+          marginBottom: 10,
+          marginRight: 12,
+          left: 5,
+          borderRadius: 6
+        }}>
+          <TouchableOpacity onPress={addMore} style={style.addTagButton}>
+            <View>
+              <Text style={{ fontSize: 17, margin: 5, color: "white" }}>Add Label</Text>
+            </View>
+          </TouchableOpacity>
         </View>
-      </View>
+      </ScrollView>
       <TouchableOpacity
         style={style.button}
+        activeOpacity={1}
         onPress={() => {
-          console.log(state);
+          var tagsData = []
+          var chosenTags = selectedTags.arr
+          tags.forEach(function (tag, index) {
+            if (chosenTags[index] == true) {
+              tagsData.push(tag)
+            }
+          })
+          customTags.forEach(function (tag) {
+            if (tag != "") {
+              tagsData.push(tag)
+            }
+          })
+          console.log(tagsData)
           var metadata = new Metadata(
             state.user,
             state.user,
@@ -75,80 +200,132 @@ function UploadScreen({ route, navigation }) {
           );
           var image = new Image_object(
             img.uri,
-            [category1, category2, category3],
+            tagsData,
             metadata
           );
           navigation.navigate("Confirmation", { image: image });
         }}
       >
-        <Text style={style.buttonText}>UPLOAD</Text>
+        <Text style={style.uploadButtonText}>NEXT</Text>
       </TouchableOpacity>
-      <NextButton
-        style={style.back}
-        navigation={navigation}
-        txt="CANCEL"
-        next="Home"
-      />
-    </SafeAreaView>
+      <TouchableOpacity
+        style={style.cancelButton}
+        onPress={() => navigation.navigate("Capture")}
+      >
+        <Text style={style.cancelButtonText}>CANCEL</Text>
+      </TouchableOpacity>
+    </SafeAreaView >
   );
 }
 
 const style = StyleSheet.create({
-  input: {
-    height: 50,
-    left: 15,
-    color: "black",
+  textInputStyle: {
+    height: 45,
+    width: 69,
+    marginTop: 10,
+    marginBottom: 10,
+    marginRight: 12,
+    left: 5,
+    borderRadius: 7,
+    borderWidth: 3,
+    borderColor: "#0F2B64",
+    color: "#0F2B64",
+    backgroundColor: "white",
+    fontSize: 8
   },
-  buttonText: {
+  scrollViewHeader: {
+    fontSize: 18,
+    color: "#333333",
+    fontWeight: "500"
+  },
+  tagButton: {
+    alignItems: "center",
+    backgroundColor: "white",
+    padding: 7,
+    borderRadius: 7,
+    borderWidth: 3,
+    borderColor: "#0F2B64"
+  },
+  selectedTagButton: {
+    backgroundColor: "#0F2B64",
+    padding: 7,
+    borderRadius: 7,
+    borderWidth: 3,
+    color: "white",
+    borderColor: "#0F2B64",
+    alignItems: "center",
+  },
+  addTagButton: {
+    backgroundColor: "#0F2B64",
+    padding: 7,
+    width: 100,
+    height: 45,
+    marginLeft: 10,
+    borderRadius: 10,
+    marginVertical: 10,
+    alignItems: "center"
+  },
+  scrollViewHeaderView: {
+    marginTop: 23,
+    marginBottom: 25,
+    marginLeft: 5,
+    fontWeight: "bold",
+  },
+  uploadButtonText: {
     fontSize: 20,
-    color: "#000",
+    color: "white",
+  },
+  cancelButtonText: {
+    fontSize: 20,
+    color: "#0F2B64",
   },
   rectangle: {
-    height: 50,
-    width: 150,
-    marginTop: 30,
-    marginBottom: 30,
-    backgroundColor: "#8C7F7F",
-    position: "absolute",
-    left: 20,
-    zIndex: -1,
+    height: 45,
+    width: 69,
+    marginTop: 10,
+    marginBottom: 10,
+    marginRight: 12,
+    left: 5,
+    borderRadius: 6
   },
-  rectangle2: {
-    height: 50,
-    width: 150,
-    marginTop: 30,
-    marginBottom: 30,
-    backgroundColor: "#8C7F7F",
-    position: "absolute",
-    left: 20,
-    top: 100,
-    zIndex: -1,
+  cancelButton: {
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 10,
+    borderWidth: 5,
+    width: 350,
+    borderColor: "#0F2B64",
+    alignSelf: "center",
+    marginVertical: 10,
+    alignItems: "center"
   },
-  rectangle3: {
-    height: 50,
-    width: 150,
-    marginTop: 30,
-    marginBottom: 30,
-    backgroundColor: "#8C7F7F",
-    position: "absolute",
-    left: 20,
-    top: 200,
-    zIndex: -1,
+  selectedRectangle: {
+    height: 34,
+    width: 69,
+    marginTop: 10,
+    marginBottom: 10,
+    marginRight: 12,
+    backgroundColor: "#0F2B64",
+    color: "white",
+    left: 5,
+    borderRadius: 6,
+  },
+  selectedTagText: {
+    fontSize: 12,
+    backgroundColor: "#0F2B64",
+    color: "white",
+    borderRadius: 10
   },
   tags_label: {
     paddingTop: 10,
     marginBottom: 10,
     fontSize: 23,
   },
-  tags_view: {
+  scrollView: {
     flex: 1,
-    minWidth: 300,
+    maxWidth: 330,
     maxHeight: 300,
-    borderRadius: 5,
-    borderWidth: 2,
-    borderColor: "black",
-    borderStyle: "solid",
-    backgroundColor: "white",
+    backgroundColor: "#D8EDFA",
   },
   back: {
     backgroundColor: "white",
@@ -160,12 +337,12 @@ const style = StyleSheet.create({
     fontWeight: "bold",
   },
   button: {
-    backgroundColor: "#C4C4C4",
+    backgroundColor: "#0F2B64",
     padding: 20,
-    borderRadius: 5,
-    alignSelf: "stretch",
-    marginHorizontal: 20,
+    borderRadius: 10,
+    alignSelf: "center",
     marginVertical: 10,
+    width: 350,
     alignItems: "center",
   },
   view: {
@@ -175,13 +352,28 @@ const style = StyleSheet.create({
   label: {
     paddingTop: 10,
     marginBottom: 15,
-    fontSize: 20,
-    fontWeight: "bold",
+    fontSize: 45,
+    color: "#0F2B64",
   },
   thumbnail: {
     width: 100,
     height: 100,
+    marginBottom: 20
   },
+  tagText: {
+    fontSize: 12,
+    backgroundColor: "white",
+    color: "#0F2B64",
+    borderRadius: 10
+  },
+  textInput: {
+    fontSize: 12,
+    backgroundColor: "white",
+    color: "#0F2B64",
+    borderRadius: 10,
+    height: 16,
+    width: 60
+  }
 });
 
 export default UploadScreen;
