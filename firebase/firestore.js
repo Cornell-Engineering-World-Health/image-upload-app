c1f5212d4c13b45f8b8f9ba26a22bc0bc812d3d6
Reeti Mangal <reetm@me.com>
2021-11-15 16:23:32 -0500

    testing push again

--- firebase/firestore.js
+++ firebase/firestore.js
@@ -25,13 +25,6 @@ export const addImageToUser = (task, imagePath, email) => {
   });
 };
 
-export const addTnToUser = (task, imagePath, email) => {
-  var ref = db.collection("users").doc(email);
-  ref.update({
-    thumbnails: firebase.firestore.FieldValue.arrayUnion(imagePath),
-  });
-};
-
 export const getLabelsForTask = (task) => {
   return db.collection("tasks").doc(task).get();
 }
